e679cd0eed21a1c039e7464df658ec31a4072eba
zeeshan jaffer <zeeshan.jaffer99@gmail.com>
2023-07-21 16:34:34 +0500

    code up to date

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Hit the url', async ({ page }) => {
-  await page.goto('https://www.thecrazycouponscc.com/us/');
+  await page.goto('https://www.thecrazycoupons.com/us/');
   await page.close();
   
 });
@@ -9,12 +9,94 @@ test('Hit the url', async ({ page }) => {
 
 test('Verify All Sliders ', async ({ page }) => {
   await page.goto('https://www.thecrazycoupons.com/us/');
-  await page.locator('xpath=//li[2]//div[1]//div[1]//a[1]//img[1]').click();
-  await page.goto('https://www.cheapoair.com/flights/affiliates/independenceday?irclickid=0utx6M0qgxyPW7xXt4VF2RVFUkFxrpQ:YTW1wU0&irgwc=1&FpAffiliate=imra&FpSub=1374783_3487246_&utm_source=AFN&utm_medium=imra&utm_campaign=independenceday');
- await page.waitForTimeout(2000); 
-  //const pageTitle = await page.title();
- // expect(pageTitle).toBe('Crazy Coupons - Deals, Discounts, Coupon Codes, & Promo Codes July 2023');
- await expect(page).toHaveTitle("Save on Independence Day Travel Deals!");
-  await page.close();
-  
+  await page.locator('xpath=//li[1]//div[1]//div[1]//a[1]//img[1]').click();
+ await page.locator('xpath=//li[2]//div[1]//div[1]//a[1]//img[1]').click();
+ await page.locator('xpath=//li[3]//div[1]//div[1]//a[1]//img[1]').click();
+ await page.locator('xpath=//li[4]//div[1]//div[1]//a[1]//img[1]').click();
+ await page.locator('xpath=//li[5]//div[1]//div[1]//a[1]//img[1]').click();
+ await page.locator('xpath=//li[6]//div[1]//div[1]//a[1]//img[1]').click();
+ await page.locator('xpath=//li[7]//div[1]//div[1]//a[1]//img[1]').click();
+ await page.locator('xpath=//li[8]//div[1]//div[1]//a[1]//img[1]').click();
+ await page.locator('xpath=//li[9]//div[1]//div[1]//a[1]//img[1]').click();
+ await page.locator('xpath=//li[10]//div[1]//div[1]//a[1]//img[1]').click();
+ await page.close();
+
 });
+
+test('Sliding the sliders', async ({ page }) => {
+    await page.goto('https://www.thecrazycoupons.com/us/');
+    await page.locator('xpath=/html[1]/body[1]/div[1]/main[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/button[1]').click();
+    await page.waitForTimeout(1000);
+    await page.locator('xpath=/html[1]/body[1]/div[1]/main[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/button[2]').click();
+    await page.waitForTimeout(1000);
+    await page.locator('xpath=/html[1]/body[1]/div[1]/main[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/button[2]').click();
+    await page.waitForTimeout(1000);
+    await page.locator('xpath=/html[1]/body[1]/div[1]/main[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/button[2]').click();
+    await page.waitForTimeout(1000);
+  
+    await page.close();
+    
+  });
+
+
+
+test('View All Stores', async ({ page }) => {
+    await page.goto('https://www.thecrazycoupons.com/us/');
+  await page.locator('xpath=/html[1]/body[1]/div[1]/header[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[1]/div[1]/a[1]').click();
+  await page.waitForTimeout(1000);
+  await page.locator('xpath=/html[1]/body[1]/div[1]/header[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[1]/div[1]/ul[1]/li[13]/a[1]').click();
+  await page.waitForTimeout(2000);
+  await page.waitForTimeout(1000);
+  const scrollPixels = 5000;
+        await page.waitForTimeout(3000); // Replace with the number of pixels you want to scroll
+        await page.evaluate((scrollPixels) => {
+          window.scrollBy(5000, scrollPixels);
+        }, scrollPixels);
+        await page.close();
+    });
+
+
+test('View All Categories', async ({ page }) => {
+      await page.goto('https://www.thecrazycoupons.com/us/');
+    await page.locator('xpath=/html[1]/body[1]/div[1]/header[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[2]/div[1]/a[1]').click();
+    await page.waitForTimeout(1000);
+    await page.locator('xpath=/html[1]/body[1]/div[1]/header[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[2]/div[1]/ul[1]/li[13]/a[1]').click();
+    await page.waitForTimeout(5000);
+    const scrollPixels = 5000;
+        await page.waitForTimeout(5000); // Replace with the number of pixels you want to scroll
+        await page.evaluate((scrollPixels) => {
+          window.scrollBy(5000, scrollPixels);
+        }, scrollPixels);
+        await page.close();
+      });
+      
+
+test('View All Seasonal Deals', async ({ page }) => {
+        await page.goto('https://www.thecrazycoupons.com/us/');
+      
+        await page.locator('xpath=/html[1]/body[1]/div[1]/header[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[3]/div[1]/a[1]').click();
+        await page.waitForTimeout(1000);
+        await page.locator('xpath=/html[1]/body[1]/div[1]/header[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[3]/div[1]/ul[1]/li[4]/a[1]').click();
+        await page.waitForTimeout(5000);
+      
+        const scrollPixels = 5000;
+        await page.waitForTimeout(3000); // Replace with the number of pixels you want to scroll
+        await page.evaluate((scrollPixels) => {
+          window.scrollBy(5000, scrollPixels);
+        }, scrollPixels);
+        await page.close();
+      });
+      
+test('View All Blogs', async ({ page }) => {
+        await page.goto('https://www.thecrazycoupons.com/us/');
+        await page.locator('xpath=/html[1]/body[1]/div[1]/header[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[4]/a[1]').click();
+        await page.waitForTimeout(2000);
+        const scrollPixels = 5000;
+        await page.waitForTimeout(3000); // Replace with the number of pixels you want to scroll
+        await page.evaluate((scrollPixels) => {
+          window.scrollBy(5000, scrollPixels);
+        }, scrollPixels);
+        await page.close();
+      });
+ 
+
